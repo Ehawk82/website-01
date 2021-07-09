09c4892b3a035094f3ac0c3d78ad793f5dc70f07
Jason Graziano <mrehawk82@gmail.com>
2021-07-09 14:40:28 -0400

    UI started

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -2,16 +2,31 @@ const init = () => {
 	buildUI();
 },
 buildUI = () => {
-	const heading = createEle("h1"),
+	const heading = createEle("header"),
+		  frame_r = createEle("div"),
+		  frame_l = createEle("div"),
+		  content_frame = createEle("div"),
 		  content = createEle("div"),
 	      footer = createEle("footer");
 	//
 	heading.innerHTML = "Heading";
 
-	content.innerHTML = "content";
+	//
+	frame_r.className = "frame_r";
+	frame_r.innerHTML = "frame_r";
 
-	footer.innerHTML = "footer";
+	frame_l.className = "frame_l";
+	frame_l.innerHTML = "frame_l";
 
+	content_frame.className = "content_frame";
+	content_frame.append(frame_l,frame_r);
+
+	content.append(content_frame);
+	
+	//
+	footer.innerHTML = "EHAWK LLC 2021";
+
+	////
 	body.append(heading,content,footer);
 };
 
