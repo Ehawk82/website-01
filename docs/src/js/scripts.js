fb8faf286b7ccda6d008da6e27f0a3f8f3924375
Jason Graziano <mrehawk82@gmail.com>
2021-07-13 19:28:06 -0400

    undo

--- docs/src/js/scripts.js
+++ docs/src/js/scripts.js
@@ -3,25 +3,15 @@ const init = () => {
 },
 buildUI = () => {
 	const heading = createEle("header"),
-		  frame_r = createEle("div"),
-		  frame_l = createEle("div"),
-		  content_frame = createEle("div"),
 		  content = createEle("div"),
 	      footer = createEle("footer");
 	//
 	heading.innerHTML = "Heading";
 
 	//
-	frame_r.className = "frame_r";
-	frame_r.innerHTML = "frame_r";
 
-	frame_l.className = "frame_l";
-	frame_l.innerHTML = "frame_l";
-
-	content_frame.className = "content_frame";
-	content_frame.append(frame_l,frame_r);
-
-	content.append(content_frame);
+	content.innerHTML = "content";
+	content.className = "content";
 	
 	//
 	footer.innerHTML = "EHAWK LLC 2021";
